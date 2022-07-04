59ae9bdfb07fd32ada98e894a228818360a6e31c
dp221125 <dp221125@naver.com>
2022-07-05 00:07:37 +0900

    GamzaFoundation-iOS을 추가합니다.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/CombineCommunity/CombineExt.git", from: "1.0.0"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "7.2.0"),
+        .package(url: "https://github.com/good-potatos/GamzaFoundation-iOS", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
