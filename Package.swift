7384e5493f599acb13f05786f61b68444766152d
dp221125 <dp221125@naver.com>
2022-07-07 00:39:43 +0900

    GamzaFoundation을 추가합니다.

--- Package.swift
+++ Package.swift
@@ -13,14 +13,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/CombineCommunity/CombineExt.git", from: "1.0.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.2.0")
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.2.0"),
+        .package(url: "https://github.com/good-potatos/GamzaFoundation", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
             name: "GamzaUIKit",
             dependencies: [
                 .product(name: "CombineExt", package: "CombineExt"),
-                .product(name: "Kingfisher", package: "Kingfisher")
+                .product(name: "Kingfisher", package: "Kingfisher"),
+                .product(name: "GamzaFoundation", package: "GamzaFoundation")
             ],
             path: "GamzaUIKit",
             exclude: ["../Example"]
